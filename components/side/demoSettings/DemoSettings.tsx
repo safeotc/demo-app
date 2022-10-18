449ff8f2db11601f2a7cef09214e1e9b17162105
Dorde Kesic <dordekesic.keso@gmail.com>
2022-10-18 12:40:54 +0200

    updates demosettings component

--- components/side/demoSettings/DemoSettings.tsx
+++ components/side/demoSettings/DemoSettings.tsx
@@ -1,7 +1,9 @@
 import { useContext } from 'react';
+import InfoAlert from '../../alerts/types/InfoAlert';
 import WarningAlert from '../../alerts/types/WarningAlert';
 import { DemoContext } from '../../demo/DemoProvider';
 import { DEMO_WALLETS } from '../../demo/useDemo';
+import Button from '../../forms/buttons/Button';
 import SecondaryButton from '../../forms/buttons/SecondaryButton';
 import Radios, { NullableRadioValue, RadioOption } from '../../forms/radios/Radios';
 import FlatIcon from '../../icons/FlatIcon';
@@ -34,13 +36,27 @@ const DemoSettings = () => {
             <Modal size="m" isOpened={isOpened} onCloseRequest={hideModal}>
                 <ModalTitle>Demo application settings</ModalTitle>
 
-                <h6 className="u-margin-bottom-tiny c-modal-container__sub-title">Wallet to connect with</h6>
-                <p className="u-margin-bottom">
-                    Select the wallet you want to connect to the application. The selected wallet will be used for
-                    making/matching orders.
-                </p>
+                <div className="u-margin-bottom-large">
+                    <h6 className="u-margin-bottom-tiny c-modal-container__sub-title">Demo progress</h6>
+                    <ul className="u-margin-bottom-none">
+                        <li>Connect a wallet.</li>
+                        <li>Place a buy / sell order with the connected wallet.</li>
+                        <li>Disconnect the currently used wallet.</li>
+                        <li>
+                            Switch to another wallet and connect it in order to match the order you&apos;ve just
+                            created.
+                        </li>
+                    </ul>
+                    <p className="u-margin-bottom-none">-- todo: progress bar --</p>
+                </div>
 
                 <div className="u-margin-bottom-large">
+                    <h6 className="u-margin-bottom-tiny c-modal-container__sub-title">Wallet to connect with</h6>
+                    <p className="u-margin-bottom">
+                        Select the wallet you want to connect to the application. The selected wallet will be used for
+                        making/matching orders.
+                    </p>
+
                     <Radios
                         name="wallet-selection"
                         options={demoWallets}
@@ -58,19 +74,16 @@ const DemoSettings = () => {
                     )}
                 </div>
 
-                <h6 className="u-margin-bottom-tiny c-modal-container__sub-title">Voting simulation</h6>
-                <p className="u-margin-bottom-large">-- todo: make voting simulator to handle order governance --</p>
-
-                <h6 className="u-margin-bottom-tiny c-modal-container__sub-title">Demo progress</h6>
-                <ul className="u-margin-bottom-none">
-                    <li>Connect a wallet.</li>
-                    <li>Place a buy / sell order with the connected wallet.</li>
-                    <li>Disconnect the currently used wallet.</li>
-                    <li>
-                        Switch to another wallet and connect it in order to match the order you&apos;ve just created.
-                    </li>
-                </ul>
-                <p className="u-margin-bottom-none">-- todo: progress bar --</p>
+                <div>
+                    <h6 className="u-margin-bottom-tiny c-modal-container__sub-title">Voting simulation</h6>
+                    <Button disabled={true}>Simulate order voting</Button>
+                    <InfoAlert
+                        hideCloseButton={true}
+                        id="voting-simulation-alert"
+                        className="u-margin-top"
+                        content="Place an order and match it to enable voting simulation."
+                    />
+                </div>
             </Modal>
         </>
     );
