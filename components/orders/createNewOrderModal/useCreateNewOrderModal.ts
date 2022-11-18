0145a997f856979ca1f6f4e6cefb2d29ab730bd3
Dorde Kesic <dordekesic.keso@gmail.com>
2022-11-18 15:48:48 +0100

    adds demo step 2 completion detection

--- components/orders/createNewOrderModal/useCreateNewOrderModal.ts
+++ components/orders/createNewOrderModal/useCreateNewOrderModal.ts
@@ -1,5 +1,6 @@
 import { useCallback, useContext, useState } from 'react';
 import { AlertsContext } from '../../alerts/AlertsProvider';
+import { DemoContext } from '../../demo/DemoProvider';
 import { OnProcessed } from './createNewOrderForm/CreateNewOrderForm';
 
 type CreateNewOrderModalTab = 'buy' | 'sell';
@@ -10,10 +11,16 @@ const useCreateNewOrderModal = (closeModal: () => void) => {
     const changeTab = useCallback((tab: CreateNewOrderModalTab) => () => setActiveTab(tab), [setActiveTab]);
 
     const { addSuccessAlert, addDangerAlert } = useContext(AlertsContext);
+    const { finishStep } = useContext(DemoContext);
     const showResultAndCloseModalOnSuccess: OnProcessed = (success) => {
         if (success) {
             closeModal();
             addSuccessAlert('Order was added successfully.');
+            finishStep(
+                'create_order',
+                2,
+                'Wow! Nice... You have just created an order. Now it is time for you to pretend you are someone else. Switch to another wallet and accept the order you have just created.'
+            );
             return;
         }
         addDangerAlert('There was an error while creating an order.');
