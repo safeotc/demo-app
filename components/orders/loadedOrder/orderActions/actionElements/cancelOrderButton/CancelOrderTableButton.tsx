054fe5d03a7c89c5a1032e7b1f8dda0b94bbe343
Dorde Kesic <dordekesic.keso@gmail.com>
2023-03-21 08:34:47 +0100

    adds short version of cancel button to orders table

--- components/orders/loadedOrder/orderActions/actionElements/cancelOrderButton/CancelOrderTableButton.tsx
+++ components/orders/loadedOrder/orderActions/actionElements/cancelOrderButton/CancelOrderTableButton.tsx
@@ -1,11 +1,25 @@
-import CancelOrderButton from './CancelOrderButton';
+import DangerButton from '../../../../../forms/buttons/DangerButton';
+import FlatIcon from '../../../../../icons/FlatIcon';
+import useCancelOrderButton from './useCancelOrderButton';
 
 interface CancelOrderTableButtonProps {
     orderId: string;
 }
 
 const CancelOrderTableButton = ({ orderId }: CancelOrderTableButtonProps) => {
-    return <CancelOrderButton orderId={orderId} />;
+    const { isLoading, cancelOrder } = useCancelOrderButton(orderId);
+
+    return (
+        <DangerButton
+            className="u-width-full"
+            icon={<FlatIcon icon="trash" />}
+            disabled={isLoading}
+            loading={isLoading}
+            onClick={cancelOrder}
+        >
+            Cancel
+        </DangerButton>
+    );
 };
 
 export default CancelOrderTableButton;
